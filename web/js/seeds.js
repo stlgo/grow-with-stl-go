ce595eefc6fd3668ea5e95f8e9b5308caa7eaebd
aschiefe <aschiefe@github.com>
2024-02-25 13:22:28 -0600

    main display working

--- web/js/seeds.js
+++ web/js/seeds.js
@@ -30,6 +30,99 @@ class Seeds {
         });
     }
 
+    showSeeds(data) {
+        const div = document.getElementById('SeedsDiv');
+        Object.keys(data).forEach((category) => {
+            let heading = document.createElement('h2');
+            heading.innerHTML = category;
+            div.appendChild(heading);
+            if (Object.prototype.hasOwnProperty.call(data[category], 'items')) {
+                let table = document.createElement('table');
+                table.setAttribute('width', '99%');
+                table.classList.add('display', 'responsive', 'seeds', 'pictures');
+                let tb = table.createTBody();
+                let tr = tb.insertRow(-1);
+                data[category].items.forEach((seed) => {
+                    let seedDiv = document.createElement('div');
+                    let h3 = document.createElement('h3');
+                    h3.innerHTML = seed.commonName;
+
+                    let img = document.createElement('img');
+                    img.classList.add('rounded');
+                    img.src = seed.image;
+
+                    seedDiv.appendChild(h3);
+                    seedDiv.appendChild(img);
+                    tr.insertCell(-1).appendChild(seedDiv);
+                });
+                div.appendChild(table);
+            } else {
+                let p = document.createElement('paragraph');
+                p.innerHTML = `No ${category} found in inventory`;
+                div.appendChild(heading);
+            }
+        });
+        // const table = document.createElement('table');
+        // table.setAttribute('width', '99%');
+        // table.id = 'UsersTable';
+        // table.classList.add('display', 'responsive', 'seeds');
+        // let th = table.createTHead();
+        // let tr = th.insertRow(-1);
+        // headers.forEach((header) => {
+        //     tr.insertCell(-1).innerHTML = header;
+        // });
+        // let tb = table.createTBody();
+        // Object.keys(data).forEach((userID) => {
+        //     tr = tb.insertRow(-1);
+        //     tr.insertCell(-1).innerHTML = `<div id="${userID}-details"><i class="fa fa-plus-circle fa-lg details-control" style="color:green"></i></div>`;
+        //     tr.insertCell(-1).innerHTML = userID;
+        //     if (data[userID].lastLogin === null) {
+        //         tr.insertCell(-1).innerHTML = 'Never Logged In';
+        //     } else {
+        //         tr.insertCell(-1).innerHTML = new Date(data[userID].lastLogin).toLocaleString('en-us', { timeZoneName: 'short' });
+        //     }
+
+
+        //     let activeChk = document.createElement('input');
+        //     activeChk.type = 'checkbox';
+        //     activeChk.id = `${userID}_active_chk`;
+        //     activeChk.name = userID;
+        //     activeChk.classList.add('user-active-chk');
+        //     activeChk.checked = data[userID].active;
+        //     tr.insertCell(-1).appendChild(activeChk);
+
+        //     let adminChk = document.createElement('input');
+        //     adminChk.type = 'checkbox';
+        //     adminChk.id = `${userID}_admin_chk`;
+        //     adminChk.name = userID;
+        //     adminChk.classList.add('user-admin-chk');
+        //     adminChk.checked = data[userID].admin;
+        //     tr.insertCell(-1).appendChild(adminChk);
+
+        //     tr.insertCell(-1).innerHTML = '<i class="fa fa-trash fa-lg remove-user" style="color:maroon"></i>';
+        // });
+        // let div = document.getElementById('CurrentUsersDiv');
+        // div.innerHTML = '';
+        // div.appendChild(table);
+
+        // let userTable = $('#UsersTable').DataTable({
+        //     deferRender: true,
+        //     orderClasses: false,
+        //     columnDefs: [ {
+        //         targets: 0,
+        //         orderable: false,
+        //     }, {
+        //         targets: 4,
+        //         orderable: false,
+        //     } ]
+        // });
+
+        // this.bindActiveToggle();
+        // this.bindAdminToggle();
+        // this.bindSlideOut(userTable);
+        // this.bindRemoveUser(userTable);
+    }
+
     handleMessage(json) {
         if (Object.prototype.hasOwnProperty.call(json, 'error')) {
             this.log.error(json.error);
@@ -37,7 +130,7 @@ class Seeds {
         } else {
             switch(json.component) {
             case 'getInventory':
-                this.log.info(JSON.stringify(json));
+                this.showSeeds(json.data);
                 break;
             default:
                 this.log.error(`Cannot handle component '${json.component}' for ${this.type}`);
